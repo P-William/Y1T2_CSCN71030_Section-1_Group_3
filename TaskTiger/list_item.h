df4a2fe0ff2ca186119b88b2614c0302c84399a3
P-William <w.paetz@uzwelo.com>
2024-03-08 21:52:49 -0500

    List items

--- TaskTiger/list_item.h
+++ TaskTiger/list_item.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include "task.h"
+#include "folder.h"
+
+typedef enum Type {
+	NONE,
+	TASK,
+	FOLDER
+} Type;
+
+typedef union Item {
+	Task task;
+	Folder Folder;
+} Item;
+
+typedef struct ListItem {
+	Type type;
+	Item item;
+} ListItem;
+
+
+ListItem createListItemFromTask(Task);
+ListItem createListItemFromFolder(Folder);
+
+bool equalListItem(ListItem, ListItem);
+
+ListItem copyListItem(ListItem src);
+bool copyListItemInPlace(ListItem* dest, ListItem src);
+
+void debugPrintListItem(ListItem);
