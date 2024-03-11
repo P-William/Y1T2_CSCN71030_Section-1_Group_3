650ebc9c8ec5f04513f80f89c4023b29d8e6b9aa
P-William <w.paetz@uzwelo.com>
2024-03-10 21:13:34 -0400

    Created files

---
+++ TaskTiger/list_api.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "list"
+
+
+// These could be useful but fall more into API:
+// append task
+// append folder
+// insert task
+// insert folder
+// remove task
+// remove folder
+// set task
+// set folder
+// contains task
+// contains folder
