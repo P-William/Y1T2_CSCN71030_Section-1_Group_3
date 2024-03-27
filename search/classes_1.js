2ab99515c8173e0cc362d1fae221f158a07a3e84
P-William <w.paetz@uzwelo.com>
2024-03-27 16:09:58 -0400

    Docs Setup

---
+++ search/classes_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['folder_0',['Folder',['../struct_folder.html',1,'']]],
+  ['folderlist_1',['FolderList',['../struct_folder_list.html',1,'']]],
+  ['foldernode_2',['FolderNode',['../struct_folder_node.html',1,'']]]
+];
